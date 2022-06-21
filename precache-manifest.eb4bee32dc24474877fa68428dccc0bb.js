0448e46541282c8d4092680a8b4a240f86df160e
Yusuf Taqiyuddin <yusuftaqiyuddin1@gmail.com>
2022-06-21 20:09:48 +0700

    Updates

---
+++ precache-manifest.eb4bee32dc24474877fa68428dccc0bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "761244b1b0df36b602bf75b6bf08a71d",
+    "url": "/tugas-todolist/index.html"
+  },
+  {
+    "revision": "105a37f08a63b6bbb3e2",
+    "url": "/tugas-todolist/static/css/main.ee7d5c05.chunk.css"
+  },
+  {
+    "revision": "c268d54d421ccb44cfee",
+    "url": "/tugas-todolist/static/js/2.caa1f90d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/tugas-todolist/static/js/2.caa1f90d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "105a37f08a63b6bbb3e2",
+    "url": "/tugas-todolist/static/js/main.71d8ead5.chunk.js"
+  },
+  {
+    "revision": "31973b2396a07b88ddee",
+    "url": "/tugas-todolist/static/js/runtime-main.abc45d64.js"
+  }
+]);
